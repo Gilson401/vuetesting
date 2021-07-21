6af564d7510e8f6773e22694958063d814f26e07
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2021-07-21 16:36:56 -0300

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+      extend: {
+        outline: {
+          blue: '2px solid #0000ff',
+        },
+        cursor: {
+            auto: 'auto',
+            default: 'default',
+            pointer: 'pointer',
+           wait: 'wait',
+            text: 'text',
+           move: 'move',
+            'not-allowed': 'not-allowed',
+           crosshair: 'crosshair',
+           'zoom-in': 'zoom-in',
+          }
+      }
+    }
+  }
